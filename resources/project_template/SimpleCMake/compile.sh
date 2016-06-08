b09516d8c7816bfe3a2f883804c02522cdfaefa3
cen5bin <cen5bin@163.com>
2016-06-08 07:10:49 +0000

    修改compile.sh

--- resources/project_template/SimpleCMake/compile.sh
+++ resources/project_template/SimpleCMake/compile.sh
@@ -1,11 +1,12 @@
 rm -f run
-build=`dirname $0`/Build/Debug
+build=`dirname $0`/build/debug
 args="-DCMAKE_BUILD_TYPE=Debug"
 if [ $# = 1 ] && [ $1 = "release" ]; then
-    build=`dirname $0`/Build/Release
+    build=`dirname $0`/build/release
     args='-DCMAKE_BUILD_TYPE=Release'
 fi
 
+mkdir -p $build
 cd $build
 cmake $args ../..
 make
